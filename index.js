a4cf2db71f5825adeb8d7c82001ef3a050861ec0
taniyamim <[jannatulhafsa500@gmail.com]>
2023-05-20 02:15:43 +0600

    id wise data generated

--- index.js
+++ index.js
@@ -37,6 +37,15 @@ async function run() {
     })
 
 
+    app.get('/toys/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await toysCollection.findOne(query);
+        res.send(result);
+    })
+
+
+
 
 
     // Send a ping to confirm a successful connection
