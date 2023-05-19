f38c86c9fd5954ebe1c831840b55d2b0cfb22ffa
taniyamim <[jannatulhafsa500@gmail.com]>
2023-05-20 01:56:04 +0600

    data inserted and toys page created

--- index.js
+++ index.js
@@ -28,12 +28,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toysCollection = client.db('toysMarketplace').collection('toys');
+    app.get('/toys', async (req, res) => {
+        const cursor = toysCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
